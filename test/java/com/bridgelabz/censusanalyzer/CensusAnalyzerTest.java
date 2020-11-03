218fa2abd117137efc10b0ddabc72fa203acd4a3
IbraheemKhaleel <ikhaleelem@gmail.com>
2020-11-03 22:07:08 +0530

    Refactored : code for testing method for checking wrong delimiter

--- test/java/com/bridgelabz/censusanalyzer/CensusAnalyzerTest.java
+++ test/java/com/bridgelabz/censusanalyzer/CensusAnalyzerTest.java
@@ -7,6 +7,9 @@ import org.junit.rules.ExpectedException;
 public class CensusAnalyzerTest {
 	private static final String INDIAN_CENSUS_CSV_FILE_PATH = "C:\\Users\\Ibrahim Khaleel\\eclipse-workspace\\CensusAnalyzer\\src\\test\\resources\\IndiaStateCensusData.csv" ;
 	private static final String WRONG_CSV_FILE_PATH = "C:\\Users\\Ibrahim Khaleel\\eclipse-workspace\\CensusAnalyzer\\src\\main\\resources\\IndiaStateCensusData.csv" ;
+	private static final String WRONG_TYPE = "C:\\Users\\Ibrahim Khaleel\\eclipse-workspace\\CensusAnalyzer\\src\\test\\resources\\IndiaStateCensusData.pdf" ;
+	private static final String EXTENSION_FORMAT = "^[a-z:\\ A-Z987]{1,}[.][c][s][v]$";
+	private static final String INDIAN_CENSUS_CSV_WRONG_DELIMITER = "C:\\Users\\Ibrahim Khaleel\\eclipse-workspace\\CensusAnalyzer\\src\\test\\resources\\WrongDelimiter.csv";
 	@Test
 	public void givenIndianCensusCSC_WhenChecked_ShouldReturnCorrectRecords() {
 		StateCensusAnalyser censusAnalyser = new StateCensusAnalyser();
@@ -18,7 +21,7 @@ public class CensusAnalyzerTest {
 		Assert.assertEquals(29, count);
 	}
 	@Test
-	public void givenIndianCensusCSC_WhenWrongFlie_ShouldThrowException()  {
+	public void givenIndianCensusCSV_WhenWrongFlie_ShouldThrowException()  {
 		try {
 		StateCensusAnalyser censusAnalyser = new StateCensusAnalyser();
 		ExpectedException exceptionRule = ExpectedException.none();
@@ -29,4 +32,24 @@ public class CensusAnalyzerTest {
 		}
 	}
 	
+	 @Test
+	    public void givenWrongDelimiter_InIndiaCensusData_ShouldReturnCustomExceptionType() {
+	        try {
+	            StateCensusAnalyser censusAnalyser = new StateCensusAnalyser();
+	            censusAnalyser.loadCensus(INDIAN_CENSUS_CSV_WRONG_DELIMITER);
+	        } catch (CensusAnalyserException e) {
+	            Assert.assertEquals(CensusAnalyserException.ExceptionType.CSV_FILE_INTERNAL_ISSUES, e.type);
+	        }
+	    }
+	
+	@Test
+	
+		public void givenWrongCSVFileType_WhenChecked_ShouldThrowException() {
+			StateCensusAnalyser censusAnalyser = new StateCensusAnalyser();
+			try {
+				censusAnalyser.patternMatching(EXTENSION_FORMAT, "WRONG_TYPE");
+		} catch (CensusAnalyserException e) {
+			Assert.assertEquals(CensusAnalyserException.ExceptionType.WRONG_TYPE, e.type);
+		}	
+	}	
 }
