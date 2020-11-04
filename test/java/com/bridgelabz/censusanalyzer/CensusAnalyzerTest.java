a4d9695af1295c9e264f1c672433d711b5ac63fc
IbraheemKhaleel <ikhaleelem@gmail.com>
2020-11-04 06:47:06 +0530

    Refactored : code for testing method for checking missing header in csv file

--- test/java/com/bridgelabz/censusanalyzer/CensusAnalyzerTest.java
+++ test/java/com/bridgelabz/censusanalyzer/CensusAnalyzerTest.java
@@ -7,9 +7,8 @@ import org.junit.rules.ExpectedException;
 public class CensusAnalyzerTest {
 	private static final String INDIAN_CENSUS_CSV_FILE_PATH = "C:\\Users\\Ibrahim Khaleel\\eclipse-workspace\\CensusAnalyzer\\src\\test\\resources\\IndiaStateCensusData.csv" ;
 	private static final String WRONG_CSV_FILE_PATH = "C:\\Users\\Ibrahim Khaleel\\eclipse-workspace\\CensusAnalyzer\\src\\main\\resources\\IndiaStateCensusData.csv" ;
-	private static final String WRONG_TYPE = "C:\\Users\\Ibrahim Khaleel\\eclipse-workspace\\CensusAnalyzer\\src\\test\\resources\\IndiaStateCensusData.pdf" ;
-	private static final String EXTENSION_FORMAT = "^[a-z:\\ A-Z987]{1,}[.][c][s][v]$";
 	private static final String INDIAN_CENSUS_CSV_WRONG_DELIMITER = "C:\\Users\\Ibrahim Khaleel\\eclipse-workspace\\CensusAnalyzer\\src\\test\\resources\\WrongDelimiter.csv";
+	private static final String INDIAN_CENSUS_CSV_MISSING_HEADER ="C:\\Users\\Ibrahim Khaleel\\eclipse-workspace\\CensusAnalyzer\\src\\test\\resources\\MissingHeader.csv" ;
 	@Test
 	public void givenIndianCensusCSC_WhenChecked_ShouldReturnCorrectRecords() {
 		StateCensusAnalyser censusAnalyser = new StateCensusAnalyser();
@@ -31,25 +30,22 @@ public class CensusAnalyzerTest {
 			Assert.assertEquals(CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM, e.type);
 		}
 	}
-	
-	 @Test
-	    public void givenWrongDelimiter_InIndiaCensusData_ShouldReturnCustomExceptionType() {
+	@Test
+	public void givenWrongDelimiter_InIndiaCensusData_ShouldReturnCustomExceptionType() {
 	        try {
 	            StateCensusAnalyser censusAnalyser = new StateCensusAnalyser();
 	            censusAnalyser.loadCensus(INDIAN_CENSUS_CSV_WRONG_DELIMITER);
 	        } catch (CensusAnalyserException e) {
 	            Assert.assertEquals(CensusAnalyserException.ExceptionType.CSV_FILE_INTERNAL_ISSUES, e.type);
 	        }
-	    }
-	
-	@Test
-	
-		public void givenWrongCSVFileType_WhenChecked_ShouldThrowException() {
-			StateCensusAnalyser censusAnalyser = new StateCensusAnalyser();
-			try {
-				censusAnalyser.patternMatching(EXTENSION_FORMAT, "WRONG_TYPE");
-		} catch (CensusAnalyserException e) {
-			Assert.assertEquals(CensusAnalyserException.ExceptionType.WRONG_TYPE, e.type);
-		}	
-	}	
+	 }
+	 @Test
+	 public void givenMissingHeader_InIndiaCensusData_ShouldReturnCustomExceptionType() {
+	        try {
+	            StateCensusAnalyser censusAnalyser = new StateCensusAnalyser();
+	            censusAnalyser.loadCensus(INDIAN_CENSUS_CSV_MISSING_HEADER);
+	        } catch (CensusAnalyserException e) {
+	            Assert.assertEquals(CensusAnalyserException.ExceptionType.CSV_FILE_INTERNAL_ISSUES, e.type);
+	        }
+	 }
 }
